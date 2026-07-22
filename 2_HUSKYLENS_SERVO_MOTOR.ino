deda0ddf1c03846aebca510bdfbdd2a4fa41db0a
ditto99-cmyk <ditto99@ai.jne.kr>
2026-07-23 05:55:38 +0900

    Add files via upload

---
+++ 2_HUSKYLENS_SERVO_MOTOR.ino
@@ -0,0 +1,63 @@
+
+#include "HUSKYLENS.h"
+#include "SoftwareSerial.h"
+#include <Servo.h>
+HUSKYLENS huskylens;
+//HUSKYLENS green line >> SDA; blue line >> SCL
+void printResult(HUSKYLENSResult result);
+
+Servo servo;;
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    while (!huskylens.begin(Wire))
+    {
+        Serial.println(F("Begin failed!"));
+        Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
+        Serial.println(F("2.Please recheck the connection."));
+        delay(100);
+    }
+    servo.attach(9);
+       servo.write(0);
+
+}
+
+void loop() {
+    if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+    else if(!huskylens.isLearned()) Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
+    else if(!huskylens.available()) Serial.println(F("No block or arrow appears on the screen!"));
+    else
+    {
+        Serial.println(F("###########"));
+        while (huskylens.available())
+        {
+            HUSKYLENSResult result = huskylens.read();
+            printResult(result);
+        }    
+    }
+}
+
+void printResult(HUSKYLENSResult result){
+    if (result.command == COMMAND_RETURN_BLOCK){
+        Serial.println(String()+F("Block:xCenter=")+result.xCenter+F(",yCenter=")+result.yCenter+F(",width=")+result.width+F(",height=")+result.height+F(",ID=")+result.ID);
+    }
+  while (result.ID == 1){
+    servo.write(90);
+   delay(1000);
+}
+ if (result.ID == 0){
+ servo.write(0);
+ delay(1000);
+} 
+  else {
+ servo.write(0);
+
+} 
+    
+ //   else{
+//        Serial.println("Object unknown!");
+ // servo.write(0);
+
+ //  }
+    }
